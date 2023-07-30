2cebe8a53ca29c02c746823dd78607c2be0004b9
Mariusz Trocewicz <throcu@gmail.com>
2023-07-30 23:33:59 +0200

    add: listContacts func

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,23 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
+const contactsPath = path.resolve('models', 'contacts.json');
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  try {
+    const getContacts = await fs.readFile(contactsPath);
+    return JSON.parse(getContacts);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async contactId => {};
 
-const removeContact = async (contactId) => {}
+const removeContact = async contactId => {};
 
-const addContact = async (body) => {}
+const addContact = async body => {};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +25,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,31 @@
-const express = require('express')
+const { listContacts } = require('../../models/contacts');
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', async (req, res) => {
+  const list = await listContacts();
+  res.send(list);
+
+  // res.send(req.body);
+  // res.sendStatus(200);
+  // res.json({ message: 'template message' });
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json({ message: 'template message' });
+});
 
-module.exports = router
+module.exports = router;
