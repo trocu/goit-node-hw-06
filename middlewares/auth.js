3c5877ccd8f4cd5cf240122f22f013003430fbbc
Mariusz Trocewicz <throcu@gmail.com>
2023-08-17 17:30:10 +0200

    fix: combined two OR operators into one

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,15 +4,7 @@ const auth = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
     const headerToken = req.headers.authorization;
     const accessToken = headerToken.split(' ')[1];
-    if (accessToken !== user.token) {
-      return res.status(401).send({
-        status: 'error',
-        code: 401,
-        data: 'Unauthorized',
-        message: 'Not authorized',
-      });
-    }
-    if (!user || err) {
+    if (!user || err || accessToken !== user.token) {
       return res.status(401).send({
         status: 'error',
         code: 401,
